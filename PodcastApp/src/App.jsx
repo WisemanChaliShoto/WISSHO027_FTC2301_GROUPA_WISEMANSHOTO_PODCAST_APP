0754d2296f4ff5a9cc834dc11337543b8f34fb1f
Wiseman Chali Shoto <Charlie.Shoto@gmail.com>
2023-08-03 07:50:27 +0100

    Final commit

--- PodcastApp/src/App.jsx
+++ PodcastApp/src/App.jsx
@@ -6,11 +6,6 @@ import Shows from './components/Home/Shows'
 import Menu from './components/Home/Menu';
 
 
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://bagwuefyfgrwfnoyrwhp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhZ3d1ZWZ5Zmdyd2Zub3lyd2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4OTE3MDAsImV4cCI6MjAwNjQ2NzcwMH0.88gE9y2nXt6llr8fETParqe0i7s675S_LvkrCW-qErw')
-
 
 import SeasonCard from './components/Shows/Season';
 import Episodes from './components/Shows/Episodes';
